c75ff322f084c2e0097fc395d3583f27bba10d71
preethi-r04 <preethiliki2004@gmail.com>
2026-01-12 12:34:10 +0530

    Add core programming files

---
+++ src/CoreProgramming/Level1/HarryAge.java
@@ -0,0 +1,11 @@
+package CoreProgramming.Level1;
+public class HarryAge {
+    public static void main(String[] args) {
+    int BirthYear = 2000;
+    int CurrentYear = 2024;
+    int Age = CurrentYear - BirthYear;
+    System.out.println("Harry's age in 2024 is "+ Age);
+    
+    }
+    
+}
---
+++ src/CoreProgramming/Level1/VolOfEarth.java
@@ -0,0 +1,12 @@
+
+package CoreProgramming.Level1;
+public class VolOfEarth {
+    public static void main(String[] args) {
+        int radiusOfEarth = 6378;
+        double vol = (4/3)* Math.PI *(radiusOfEarth * radiusOfEarth * radiusOfEarth);
+        double radiusMiles = radiusOfEarth/1.6;
+        double volMiles = (4/3)* Math.PI *(radiusMiles * radiusMiles * radiusMiles);
+        System.out.println("The volume of earth in cubic kilometers is "+vol+ " and cubic miles is "+volMiles);
+
+    }
+}
